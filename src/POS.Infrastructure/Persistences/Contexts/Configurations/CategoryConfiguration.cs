30a5c5345175c2c264b4c6a03fe42aa8cc738002
gacuma <gacuma83@gmail.com>
2023-05-17 11:49:02 -0600

    Se agregan configuraciones de cada entidad

---
+++ src/POS.Infrastructure/Persistences/Contexts/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using POS.Domain.Entities;
+
+namespace POS.Infrastructure.Persistences.Contexts.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(e => e.CategoryId).HasName("PK__Categori__19093A0B556A8131");
+
+        builder.Property(e => e.Name).HasMaxLength(100);
+    }
+}
